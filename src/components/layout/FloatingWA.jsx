18a0772bd696d6a1e473fccd11d9857d22455316
arifmursyid15 <arifmursyid15@gmail.com>
2026-05-02 14:21:54 +0700

    floating WA

--- src/components/layout/FloatingWA.jsx
+++ src/components/layout/FloatingWA.jsx
@@ -1,7 +1,7 @@
 ﻿export default function FloatingWA() {
   return (
     <div className="floating-wa">
-      <a href="https://wa.me/6281200000000" target="_blank" rel="noreferrer">
+      <a href="https://wa.me/6282177832648" target="_blank" rel="noreferrer">
         Hubungi Kami
       </a>
     </div>
